b18af45a102f77d9ca7b4cb2122803f2fde0431b
RAMYA A <125212940+ramyaa06@users.noreply.github.com>
2023-11-23 13:36:39 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const App = () => {
+  const [textColor, setTextColor] = useState('black');
+
+  const handleButtonPress = () => {
+    const randomColor = ‘red’;
+    setTextColor(randomColor);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: textColor, fontSize: 20 }}>
+        Hello, this text can change color!
+      </Text>
+      <Button title="Change Color" onPress={handleButtonPress} color="blue" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
